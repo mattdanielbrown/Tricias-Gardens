09a06ccb5dcf07c335b30fe6456d386519770a65
Matthew Daniel Brown <developer@mattdanielbrown.com>
2016-10-15 14:59:12 -0500

    Added some files (imagse ) updated mail.php file used to deal with contact form

--- src/mail/_mail__ORIGINAL-BACKUP.php
+++ src/mail/_mail__ORIGINAL-BACKUP.php
@@ -6,7 +6,7 @@
 
 // Choose here whether to use php mail() function or your SMTP server (recommended) to send the email.
 // Use 'smtp' for better reliability, or use 'phpmail' for quick + easy setup with lower reliability.
-$emailMethod                = 'phpmail'; // REQUIRED value. Options: 'smtp' , 'phpmail'
+$emailMethod                = 'smtp'; // REQUIRED value. Options: 'smtp' , 'phpmail'
 
 // Outgoing Server Settings - replace values on the right of the = sign with your own.
 // These 3 settings are only required if you choose 'smtp' for emailMethod above.
@@ -20,14 +20,14 @@ $sendingAccountUsername     = 'email@domain.com';
 $sendingAccountPassword     = 'p@55w0rd';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail             = 'contactform@triciasgardens.com'; // REQUIRED value.
-$recipientName              = 'Trica Brown';             // REQUIRED value.
+$recipientEmail             = 'email@yourdomain.com'; // REQUIRED value.
+$recipientName              = 'John Doe';             // REQUIRED value.
 
 // Email details            - Change these to suit your website needs
 $emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
-$websiteName                = 'Tricias Gardens';                // REQUIRED value. This is used when a name or email is not collected from the website form
+$websiteName                = 'Your Website';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone					= 'America/Chicago';         // OPTIONAL, but some servers require this to be set. 
+$timeZone					= 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set. 
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
  /*----------------------------------------------------------------------------*\
 |*  You do not need to edit anything below this line, the rest is automatic.    *|
